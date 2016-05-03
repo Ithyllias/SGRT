57678ab603034edf765b03d7a986931a3598484f
Alexis Demers <alexisdemers77@gmail.com>
2016-05-03 16:57:46 -0400

    Minor fix

--- resources/views/init.blade.php
+++ resources/views/init.blade.php
@@ -1,7 +1,7 @@
 <script src="{{ URL::asset('js/jquery-2.2.3.min.js') }}"></script>
 <script src="{{ URL::asset('js/services.js') }}" type="text/javascript"></script>
 <?php
-    $lang = Session::get('newLang');
+    $lang = Session::get('locale');
     if(isset($lang)){
         App::setLocale($lang);
     }
