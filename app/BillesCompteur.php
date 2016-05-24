0c9e0808c5d90c8ed0386df3d52a4812b7d0f57b
Alexis Demers <alexisdemers77@gmail.com>
2016-05-24 16:29:02 -0400

    Start eloquent

---
+++ app/BillesCompteur.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BillesCompteur extends Model
+{
+    protected $table = 'billes_compteur_bcp';
+    protected $primaryKey = 'bct_id';
+    public $timestamps = false;
+    public $incrementing = false;
+
+    public static
+}
