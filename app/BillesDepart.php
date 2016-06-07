3d1fa1e8e5a88b87a3c260dc64a4e9909ef689be
Alexis Demers <alexisdemers77@gmail.com>
2016-06-07 16:58:41 -0400

    Added method to billesdepart

--- app/BillesDepart.php
+++ app/BillesDepart.php
@@ -9,6 +9,7 @@ class BillesDepart extends Model
     protected $table = 'billes_depart_bdp';
     protected $primaryKey = 'bdp_id';
     public $timestamps = false;
+    protected $fillable = array('bdp_ens_id', 'bdp_cou_no', 'bdp_nb');
 
     public function cours(){
         return $this->belongsTo('App\Cours', 'bdp_cou_no', 'cou_no');
@@ -16,5 +17,18 @@ class BillesDepart extends Model
 
     public function enseignant(){
         return $this->belongsTo('App\Enseignant', 'bdp_ens_id', 'ens_id');
+    }
+
+    public static function addSingle($ensId, $cou_no, $billes){
+        try{
+            TacheReelle::create([
+                'bdp_ens_id' => $ensId,
+                'bdp_cou_no' => $cou_no,
+                'bdp_nb' => $billes,
+            ]);
+            return true;
+        } catch(\Exception $e) {
+            return false;
+        }
     }
 }
