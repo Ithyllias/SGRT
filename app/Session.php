90f1a45c95da8bf0f2939967c028edfd275e773c
Alexis Demers <alexisdemers77@gmail.com>
2016-06-07 15:18:31 -0400

    Renamed method

--- app/Session.php
+++ app/Session.php
@@ -10,8 +10,8 @@ class Session extends Model
     protected $primaryKey = 'ses_id';
     public $timestamps = false;
 
-    public static function getSessionIdFromIni($ini){
-        $sesId = Session::where('ses_initiale', $ini)->first()->ses_id;
+    public static function getSessionIdFromAlais($alias){
+        $sesId = Session::where('ses_initiale', $alias)->first()->ses_id;
         return $sesId == null ? -1 : $sesId;
     }
 }
